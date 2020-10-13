fac4b1cde05bcaf24ffe11329a7ffa92f6883038
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-13 12:51:48 +0000

    Finished methods example

--- Main.java
+++ Main.java
@@ -1,15 +1,44 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ * method example file
+ * @author Zach Ramirez 
  */
 public class Main {
 
+  
+  //prints hello world to the screen
+  public static void sayHello(){
+    System.out.println("hello world!"); 
+  } 
+
+  //prints hello to a person 
+  public static void sayHello(String name){
+    System.out.println("Hello" + name);
+  }
+  
+  
+  //a method to calcute the area of a rectangle given the width and the length
+  public static double areaOfRectangle( double length, double width){ 
+    double area = width * length; 
+    return area;  
+  }
+  
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   Scanner input = new Scanner(System.in); 
+   System.out.println("Please enter a width"); 
+   double width = input.nextDouble(); 
+   System.out.println("Please enter a length"); 
+   double length = input.nextDouble(); 
+   //calculate the area using the input 
+   double area = areaOfRectangle(width, length); 
+   System.out.println(area);  
+
     
+    //say hello world 
+    sayHello(); 
   }
 }
